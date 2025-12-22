db5ea1d4ebb1aaa88801b127fe0ce01aa8cec513
d3eny <62h7bg2x8b@privaterelay.appleid.com>
2025-12-22 17:34:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -931,11 +931,8 @@ document.getElementById("registerForm").addEventListener("submit", (e) => {
 
   alert("Form is valid (next: backend)");
 });
-const captchaResponse = grecaptcha.getResponse();
-if (!captchaResponse) {
-  alert("Please confirm captcha");
-  return;
-}
+
+
 
 
 
