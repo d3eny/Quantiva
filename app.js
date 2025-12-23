40b64ff406fe68434b26a68f932bbb511f96957f
Dan11l7 <130250579+Dan11l7@users.noreply.github.com>
2025-12-23 23:47:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
    - Language switch (EN/DE/RU) via data-i18n + localStorage
    - Modals (login/signup), swap, close, toast
    ========================================================= */
-const supabase = window.supabase.createClient(
+const sb = window.supabase.createClient(
   "https://towzwaximnwmkeyvthvk.supabase.co/",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvd3p3YXhpbW53bWtleXZ0aHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1MTgxMjQsImV4cCI6MjA4MjA5NDEyNH0.UcR2Vo4zQnQSmxG2TfiQvkHK9qRb_3W6g3knXG8PsrI"
 );
@@ -1026,3 +1026,4 @@ if (registerForm) {
     alert("Form is valid (next: backend)");
   });
 }
+
