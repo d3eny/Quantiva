619223e9217e258b37e64546d33543ce4d95f5b8
Dan11l7 <130250579+Dan11l7@users.noreply.github.com>
2025-12-24 00:10:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
    - Modals (login/signup), swap, close, toast
    ========================================================= */
 const sb = window.supabase.createClient(
-  "https://towzwaximnwmkeyvthvk.supabase.co/",
+  "https://towzwaximnwmkeyvthvk.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvd3p3YXhpbW53bWtleXZ0aHZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1MTgxMjQsImV4cCI6MjA4MjA5NDEyNH0.UcR2Vo4zQnQSmxG2TfiQvkHK9qRb_3W6g3knXG8PsrI"
 );
 
@@ -1026,4 +1026,5 @@ if (registerForm) {
     alert("Form is valid (next: backend)");
   });
 }
+
 
